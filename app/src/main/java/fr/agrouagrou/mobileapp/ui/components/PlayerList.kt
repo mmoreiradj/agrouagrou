d584f6a385b404b5ac0c5f6f335ad304ce65e21b
Martin Moreira de Jesus <martin.moreiradj@icloud.com>
2024-03-28 13:12:55 +0100

    feat(app): make player list item clickable

--- app/src/main/java/fr/agrouagrou/mobileapp/ui/components/PlayerList.kt
+++ app/src/main/java/fr/agrouagrou/mobileapp/ui/components/PlayerList.kt
@@ -1,8 +1,15 @@
 package fr.agrouagrou.mobileapp.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.twotone.KeyboardArrowRight
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.twotone.KeyboardArrowRight
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -19,7 +26,7 @@ import fr.agrouagrou.common.player.Role
 import fr.agrouagrou.mobileapp.roleToImage
 
 @Composable
-fun PlayerList(players: List<Player>) {
+fun PlayerList(players: List<Player>, onClick: (Player) -> Unit = {}) {
     Column {
         players.forEach { player ->
             ListItem(
@@ -41,6 +48,19 @@ fun PlayerList(players: List<Player>) {
                             .size(56.dp, 56.dp)
                     )
                 },
+                trailingContent = if (onClick != {}) {
+                    {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.TwoTone.KeyboardArrowRight,
+                            contentDescription = "Edit",
+                            Modifier
+                                .size(24.dp)
+                                .clickable { onClick(player) }
+                        )
+                    }
+                } else {
+                    null
+                }
             )
             HorizontalDivider()
         }
