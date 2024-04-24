d2f12a9fef482e0393725a9d40215a0cff661cb4
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2024-04-24 09:30:35 +0200

    feat: add navController and JoinGameScreen in navigation

--- app/src/main/java/fr/agrouagrou/mobileapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/fr/agrouagrou/mobileapp/ui/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ package fr.agrouagrou.mobileapp.ui.screens
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Add
@@ -11,18 +12,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import fr.agrouagrou.grpc_server.R
 
-@Preview
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavController) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = spacedBy(16.dp)
+        verticalArrangement = spacedBy(space = 16.dp, alignment = Alignment.CenterVertically),
+        modifier = Modifier.fillMaxSize(),
     ) {
         Text(text = stringResource(id = R.string.home_title), fontSize = 30.sp)
         Button(onClick = { /*TODO*/ }) {
@@ -33,7 +35,7 @@ fun HomeScreen() {
                 Text(text = stringResource(id = R.string.home_create_game))
             }
         }
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { navController.navigate("join-game") }) {
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
