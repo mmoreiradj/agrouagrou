d47ac5f9e759ce7982db3905f6f96d4970ba3608
Kuruyia <github@kuruyia.net>
2024-05-28 17:06:32 +0200

    feat: add debug method for setting the game state

--- common/src/main/kotlin/fr/agrouagrou/common/extensions/GameState.kt
+++ common/src/main/kotlin/fr/agrouagrou/common/extensions/GameState.kt
@@ -11,3 +11,13 @@ fun GameState.toProto() =
         GameState.NIGHT_WEREWOLF -> GameStateStatus.NIGHT_WEREWOLF
         GameState.NIGHT_WITCH -> GameStateStatus.NIGHT_WITCH
     }
+
+fun GameStateStatus.toGameState() =
+    when (this) {
+        GameStateStatus.LOOKING_FOR_PLAYERS -> GameState.LOOKING_FOR_PLAYERS
+        GameStateStatus.NIGHT_START -> GameState.NIGHT_START
+        GameStateStatus.NIGHT_FORTUNE_TELLER -> GameState.NIGHT_FORTUNE_TELLER
+        GameStateStatus.NIGHT_WEREWOLF -> GameState.NIGHT_WEREWOLF
+        GameStateStatus.NIGHT_WITCH -> GameState.NIGHT_WITCH
+        GameStateStatus.UNRECOGNIZED -> throw IllegalArgumentException("Unrecognized game state")
+    }
--- common/src/main/kotlin/fr/agrouagrou/common/service/DebugService.kt
+++ common/src/main/kotlin/fr/agrouagrou/common/service/DebugService.kt
@@ -2,11 +2,24 @@ package fr.agrouagrou.common.service
 
 import com.google.protobuf.Empty
 import fr.agrouagrou.common.GameManager
+import fr.agrouagrou.common.extensions.toGameState
+import fr.agrouagrou.common.extensions.toProto
 import fr.agrouagrou.proto.DebugGrpcKt
+import fr.agrouagrou.proto.SetGameStateReply
+import fr.agrouagrou.proto.SetGameStateRequest
+import fr.agrouagrou.proto.setGameStateReply
 
 class DebugService(private val gameManager: GameManager) : DebugGrpcKt.DebugCoroutineImplBase() {
     override suspend fun startGame(request: Empty): Empty {
         gameManager.startGame()
         return Empty.getDefaultInstance()
+    }
+
+    override suspend fun setGameState(request: SetGameStateRequest): SetGameStateReply {
+        gameManager.gameState.value = request.status.toGameState()
+
+        return setGameStateReply {
+            status = gameManager.gameState.value.toProto()
+        }
     }
 }
