ba8b531b0371f5a6a7b7e7f3a5037821a786f8d2
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-06-09 16:54:20 +0200

    feat(ui): add gameid info

--- app/src/main/java/fr/agrouagrou/mobileapp/ui/components/GameIDInfo.kt
+++ app/src/main/java/fr/agrouagrou/mobileapp/ui/components/GameIDInfo.kt
@@ -1,17 +1,27 @@
 package fr.agrouagrou.mobileapp.ui.components
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.startActivity
 import fr.agrouagrou.grpc_server.R
 
 @Composable
-fun GameIDInfo(gameID: String) {
+fun GameIDInfo(gameID: String, context: Context) {
+    val sendIntent = Intent(Intent.ACTION_SEND).apply {
+        putExtra(Intent.EXTRA_TEXT, gameID)
+        type = "text/plain"
+    }
+    val shareIntent = Intent.createChooser(sendIntent, null)
+
     Column {
         Text(
             text = stringResource(R.string.waiting_for_other_players),
@@ -21,7 +31,9 @@ fun GameIDInfo(gameID: String) {
             text = stringResource(R.string.your_game_id_is, gameID),
             color = MaterialTheme.colorScheme.primary
         )
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            startActivity(context, shareIntent, null)
+        }) {
             Text(text = stringResource(R.string.copy_game_id))
         }
     }
@@ -30,5 +42,5 @@ fun GameIDInfo(gameID: String) {
 @Preview
 @Composable
 fun GameIDInfoPreview() {
-    GameIDInfo("ABCD")
+    GameIDInfo("ABCD", LocalContext.current)
 }
