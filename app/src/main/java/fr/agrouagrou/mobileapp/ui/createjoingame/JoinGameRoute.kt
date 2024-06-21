65985cd013e8c92b28ac53686e2aa5dafe84b625
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2024-06-21 11:11:46 +0200

    fix(style): add padding and alignment

--- app/src/main/java/fr/agrouagrou/mobileapp/ui/createjoingame/JoinGameRoute.kt
+++ app/src/main/java/fr/agrouagrou/mobileapp/ui/createjoingame/JoinGameRoute.kt
@@ -1,14 +1,19 @@
 package fr.agrouagrou.mobileapp.ui.createjoingame
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.agrouagrou.grpc_server.R
 import fr.agrouagrou.mobileapp.ui.components.JoinGameForm
 
@@ -18,7 +23,9 @@ fun JoinGameRoute() {
     val gameCode = remember { mutableStateOf("") }
 
     Column(
-        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = spacedBy(space = 16.dp, alignment = Alignment.CenterVertically),
+        modifier = Modifier.fillMaxSize(),
     ) {
         Text(
             text = stringResource(R.string.join_game),
@@ -31,7 +38,10 @@ fun JoinGameRoute() {
             onUsernameChange = { username.value = it },
             onGameCodeChange = { gameCode.value = it },
             onJoinGame = { username, gameCode ->
-                Log.d("JoinGameRoute", "Joining game with username $username and game code $gameCode")
+                Log.d(
+                    "JoinGameRoute",
+                    "Joining game with username $username and game code $gameCode"
+                )
             }
         )
 
