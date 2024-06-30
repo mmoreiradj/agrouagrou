21ebf80f4431bcedfd9259fcefcf42f917a41677
Martin Moreira de Jesus <martin.moreira-de-jesus@protonmail.com>
2024-06-30 22:54:07 +0200

    feat(app): add phone tilt detection (#14)

--- app/src/main/java/fr/agrouagrou/mobileapp/ui/gameroutes/PlayerGameRoute.kt
+++ app/src/main/java/fr/agrouagrou/mobileapp/ui/gameroutes/PlayerGameRoute.kt
@@ -1,5 +1,16 @@
 package fr.agrouagrou.mobileapp.ui.gameroutes
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -7,12 +18,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.content.ContextCompat
 import fr.agrouagrou.common.GameState
 import fr.agrouagrou.common.player.Player
 import fr.agrouagrou.common.player.PlayerStatus
@@ -20,6 +36,10 @@ import fr.agrouagrou.grpc_server.R
 import fr.agrouagrou.mobileapp.toCard
 import fr.agrouagrou.mobileapp.ui.common.ClientPlayerViewModel
 import fr.agrouagrou.mobileapp.ui.gamephases.CardView
+import kotlinx.coroutines.launch
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
 @Composable
 fun GameRunningRoute(
@@ -74,8 +94,51 @@ fun LookingForPlayers() {
     }
 }
 
+private fun vibratePhone(context: Context) {
+    val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+        vibratorManager.defaultVibrator
+    } else {
+        context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+    }
+
+    val pattern = longArrayOf(0, 200)
+    vibrator.vibrate(VibrationEffect.createWaveform(pattern, -1))
+}
+
 @Composable
 fun NightScreen(player: Player) {
+    val context = LocalContext.current
+    val sensorManager = remember { ContextCompat.getSystemService(context, SensorManager::class.java)!! }
+    val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+    DisposableEffect(Unit) {
+        val listener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                event?.let {
+                    val z = it.values[2]
+
+                    if (z >= 7.0) {
+                        Log.d("NightScreen", "Screen is facing up")
+                        vibratePhone(context)
+                    } else {
+                        Log.d("NightScreen", "Screen is facing down")
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        }
+
+        if (player.status == PlayerStatus.ALIVE) {
+            sensorManager.registerListener(listener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+
+        onDispose {
+            sensorManager.unregisterListener(listener)
+        }
+    }
+
     Column(
         verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
     ) {
